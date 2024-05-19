6ada4c626dfc06a430422c547f5d83636fb48528
ArgentoAskia <askiaxiaoming@outlook.com>
2024-05-20 01:38:13 +0800

    2024.5.20 CommandToolsProviders Module Init

---
+++ CocoaToolkit-CommandToolsProviders/src/main/java/cn/argento/askia/CommandTool.java
@@ -0,0 +1,10 @@
+package cn.argento.askia;
+
+/**
+ * 第三方命令行工具参数化接口，提供了管道运行输入，输出结果解析等功能
+ * 第三方工具可以通过实现该接口来进行代码本地化！
+ *
+ */
+public interface CommandTool {
+
+}
---
+++ CocoaToolkit-CommandToolsProviders/src/main/java/cn/argento/askia/ProcessResult.java
@@ -0,0 +1,8 @@
+package cn.argento.askia;
+
+/**
+ * 管道处理结果输出结果集，所有的管道输出都会被解析成该接口的对象！
+ * 开发者可以针对工具类的返回进行结果解析！
+ */
+public interface ProcessResult {
+}
