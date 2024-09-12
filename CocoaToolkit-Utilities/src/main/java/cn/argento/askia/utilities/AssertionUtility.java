9699e50533c55cb9bca54c323fe02a3dd0feef06
ArgentoAskia <askiaxiaoming@outlook.com>
2024-09-12 23:12:34 +0800

    2024.9.12 AssertionUtility: add a lot of assertion methods

--- CocoaToolkit-Utilities/src/main/java/cn/argento/askia/utilities/AssertionUtility.java
+++ CocoaToolkit-Utilities/src/main/java/cn/argento/askia/utilities/AssertionUtility.java
@@ -1,6 +1,10 @@
 package cn.argento.askia.utilities;
 
+import cn.argento.askia.exceptions.runtime.lang.ComparableVarsRequiredRuntimeException;
+
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Comparator;
 import java.util.Objects;
 
 public class AssertionUtility {
@@ -22,6 +26,53 @@ public class AssertionUtility {
    }
 
    public static void requireFileExisted(File file){
-
+       if (!file.exists()){
+           throw new IllegalArgumentException();
+       }
    }
+
+   public static void requireObjectParamAsArrayType(Object obj){
+       if (!ArrayUtility.isArray(obj)){
+           throw new IllegalArgumentException("require array type param, but provides：" + obj.getClass() + " is not a array type");
+       }
+   }
+
+    /**
+     * 需要提供可比较类型
+     * @param obj
+     */
+   public static void requireComparableVariable(Object obj){
+       Class<?> clazz = obj.getClass();
+       if (obj.getClass().isArray()){
+           clazz = obj.getClass().getComponentType();
+       }
+       if (clazz.isPrimitive()){
+           // boolean 类型不能进行比较
+           if (clazz.getName().equalsIgnoreCase("z")){
+               throw new ComparableVarsRequiredRuntimeException(obj);
+           }
+       }
+       if (Comparable.class.isAssignableFrom(clazz)){
+           return;
+       }
+       if (Comparator.class.isAssignableFrom(clazz)){
+           return;
+       }
+       throw new ComparableVarsRequiredRuntimeException(obj);
+   }
+
+   public static <T> void AssertIndexInBound(T[] array, int currentIndex){
+       if (currentIndex >= array.length){
+           throw new ArrayIndexOutOfBoundsException("Array index out of range: " + currentIndex + ", [array max length: " + array.length + "]");
+       }
+   }
+
+   public static void AssertMaxIndex(int current, int maxIndex){
+       if (current >= maxIndex){
+           throw new IndexOutOfBoundsException("max = " + maxIndex + ", but current =" + current);
+       }
+   }
+
+
+
 }
