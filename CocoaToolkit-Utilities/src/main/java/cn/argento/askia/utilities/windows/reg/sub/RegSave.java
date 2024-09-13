80476db912a9cbb11ce37ad6ce2a2b9bdfbf4a49
ArgentoAskia <askiaxiaoming@outlook.com>
2024-09-14 00:09:57 +0800

    2024.9.14 RegUtility: RegSave SubCommands impl class

---
+++ CocoaToolkit-Utilities/src/main/java/cn/argento/askia/utilities/windows/reg/sub/RegSave.java
@@ -0,0 +1,16 @@
+package cn.argento.askia.utilities.windows.reg.sub;
+
+import cn.argento.askia.utilities.windows.reg.AbstractRegCommand;
+import cn.argento.askia.utilities.windows.reg.RegSubCommands;
+import cn.argento.askia.utilities.windows.reg.deprecated.RegCommand;
+
+public final class RegSave extends AbstractRegCommand {
+    protected RegSave(RegSubCommands commands) {
+        super(commands);
+    }
+
+    @Override
+    protected void registerArgBits() {
+
+    }
+}
