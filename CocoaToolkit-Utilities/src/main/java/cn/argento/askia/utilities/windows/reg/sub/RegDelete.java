8711ef5b806a90bc2ca687f56fec5925f2fdf15d
ArgentoAskia <askiaxiaoming@outlook.com>
2024-09-14 00:08:44 +0800

    2024.9.14 RegUtility: RegDelete SubCommands impl class

---
+++ CocoaToolkit-Utilities/src/main/java/cn/argento/askia/utilities/windows/reg/sub/RegDelete.java
@@ -0,0 +1,17 @@
+package cn.argento.askia.utilities.windows.reg.sub;
+
+import cn.argento.askia.utilities.windows.reg.AbstractRegCommand;
+import cn.argento.askia.utilities.windows.reg.RegSubCommands;
+import cn.argento.askia.utilities.windows.reg.deprecated.RegCommand;
+import cn.argento.askia.utilities.windows.reg.deprecated.RegUtility;
+
+public final class RegDelete extends AbstractRegCommand {
+    protected RegDelete(RegSubCommands commands) {
+        super(commands);
+    }
+
+    @Override
+    protected void registerArgBits() {
+
+    }
+}
