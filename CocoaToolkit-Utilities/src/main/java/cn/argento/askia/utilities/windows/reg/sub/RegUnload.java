6177ba3c482bdf45c14ac8c37b1162b2033974f7
ArgentoAskia <askiaxiaoming@outlook.com>
2024-09-14 00:10:08 +0800

    2024.9.14 RegUtility: RegUnload SubCommands impl class

---
+++ CocoaToolkit-Utilities/src/main/java/cn/argento/askia/utilities/windows/reg/sub/RegUnload.java
@@ -0,0 +1,23 @@
+package cn.argento.askia.utilities.windows.reg.sub;
+
+import cn.argento.askia.utilities.windows.reg.AbstractRegCommand;
+import cn.argento.askia.utilities.windows.reg.RegSubCommands;
+import cn.argento.askia.utilities.windows.reg.deprecated.RegCommand;
+import cn.argento.askia.utilities.windows.reg.deprecated.RegUtility;
+
+/**
+ * Reg Unload 子命令
+ * <p>
+ *
+ */
+public final class RegUnload extends AbstractRegCommand {
+
+    protected RegUnload(RegSubCommands commands) {
+        super(commands);
+    }
+
+    @Override
+    protected void registerArgBits() {
+
+    }
+}
