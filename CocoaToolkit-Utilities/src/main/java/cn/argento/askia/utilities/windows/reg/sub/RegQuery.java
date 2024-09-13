685ff4b2a06bc17228fd45a508d61a692fef4353
ArgentoAskia <askiaxiaoming@outlook.com>
2024-09-14 00:09:39 +0800

    2024.9.14 RegUtility: RegQuery SubCommands impl class

---
+++ CocoaToolkit-Utilities/src/main/java/cn/argento/askia/utilities/windows/reg/sub/RegQuery.java
@@ -0,0 +1,15 @@
+package cn.argento.askia.utilities.windows.reg.sub;
+
+import cn.argento.askia.utilities.windows.reg.AbstractRegCommand;
+import cn.argento.askia.utilities.windows.reg.RegSubCommands;
+
+public final class RegQuery extends AbstractRegCommand {
+    protected RegQuery(RegSubCommands commands) {
+        super(commands);
+    }
+
+    @Override
+    protected void registerArgBits() {
+
+    }
+}
