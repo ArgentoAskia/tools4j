f44dab7a1b31b9fb5f6462b46346cda3cee400b8
ArgentoAskia <askiaxiaoming@outlook.com>
2024-09-14 00:09:47 +0800

    2024.9.14 RegUtility: RegRestore SubCommands impl class

---
+++ CocoaToolkit-Utilities/src/main/java/cn/argento/askia/utilities/windows/reg/sub/RegRestore.java
@@ -0,0 +1,17 @@
+package cn.argento.askia.utilities.windows.reg.sub;
+
+import cn.argento.askia.utilities.windows.reg.AbstractRegCommand;
+import cn.argento.askia.utilities.windows.reg.RegSubCommands;
+import cn.argento.askia.utilities.windows.reg.deprecated.RegCommand;
+
+public final class RegRestore extends AbstractRegCommand {
+
+    protected RegRestore(RegSubCommands commands) {
+        super(commands);
+    }
+
+    @Override
+    protected void registerArgBits() {
+
+    }
+}
