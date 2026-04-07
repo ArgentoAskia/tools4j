d574bbc261405434218c4343a6cf3501b7745570
ArgentoAskia <Askiaxiaoming@outlook.com>
2026-04-07 21:24:33 +0800

    2026.4.7 更新isSorted()的所有版本

--- cocoa-utilities/src/main/java/cn/argento/askia/utilities/algorithms/ArraySortAlgorithmsUtility.java
+++ cocoa-utilities/src/main/java/cn/argento/askia/utilities/algorithms/ArraySortAlgorithmsUtility.java
@@ -1,12 +1,8 @@
 package cn.argento.askia.utilities.algorithms;
 
 import cn.argento.askia.annotations.Utility;
-import cn.argento.askia.utilities.collection.ArrayUtility;
-import cn.argento.askia.utilities.lang.AssertionUtility;
 
-import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.List;
 import java.util.Objects;
 
 /**
@@ -28,9 +24,41 @@ public class ArraySortAlgorithmsUtility {
         ASC, DESC
     }
 
-    public static void bubbleSort(Comparable<?>[] target, Order orderBy){
+    // 冒泡排序
+    public static <T extends Comparable<? super T>> void bubble(T[] target, int lo, int hi, Order orderBy){
 
     }
+
+    // 插入排序
+    public static <T extends Comparable<? super T>> void insertion(T[] target, int lo, int hi, Order orderBy){
+
+    }
+
+    // 选择排序
+    public static <T extends Comparable<? super T>> void selection(T[] target, int lo, int hi, Order orderBy){
+
+    }
+
+    // 希尔排序
+    public static <T extends Comparable<? super T>> void shell(T[] target, int lo, int hi, Order orderBy){
+
+    }
+    // 快速排序
+    public static <T extends Comparable<? super T>> void quick(T[] target, int lo, int hi, Order orderBy){
+
+    }
+    // 归并排序
+    public static <T extends Comparable<? super T>> void merge(T[] target, int lo, int hi, Order orderBy){
+
+    }
+    // 堆排序
+    public static <T extends Comparable<? super T>> void heap(T[] target, int lo, int hi, Order orderBy){
+
+    }
+
+    // 非比较排序
+
+
 
 
 
@@ -46,6 +74,13 @@ public class ArraySortAlgorithmsUtility {
      * @return 如果已排好序则返回 {@code true} 否则返回 {@code false}
      * @since 2026.4.7
      * @see ArraySortAlgorithmsUtility#isSorted(Object[], Comparator, Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(int[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(byte[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(char[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(long[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(float[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(short[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(double[], Order)
      */
     public static <T extends Comparable<? super T>> boolean isSorted(T[] target, Order orderBy){
         Objects.requireNonNull(target, "提供的数组不能为NULL");
@@ -65,6 +100,237 @@ public class ArraySortAlgorithmsUtility {
     }
 
     /**
+     * 判断一个double数组是否已经排好序.
+     *
+     * @param target 待排序的double类型数组
+     * @param orderBy {@link Order#ASC}代表升序, {@link Order#DESC}代表降序
+     * @return 如果已排好序则返回 {@code true} 否则返回 {@code false}
+     * @since 2026.4.7
+     * @see ArraySortAlgorithmsUtility#isSorted(Comparable[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(Object[], Comparator, Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(byte[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(int[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(short[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(long[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(float[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(char[], Order)
+     */
+    public static boolean isSorted(double[] target, Order orderBy){
+        Objects.requireNonNull(target, "提供的数组不能为NULL");
+        boolean sorted = true;
+        for (int i = 0, j = i + 1; j < target.length; j++, i++) {
+            if (orderBy == Order.ASC && Double.compare(target[i], target[j]) > 0){
+                // 升序, 发现前面的大于后面的, 则sorted = false
+                sorted = false;
+                break;
+            }
+            if (orderBy == Order.DESC && Double.compare(target[i], target[j]) < 0){
+                sorted = false;
+                break;
+            }
+        }
+        return sorted;
+    }
+
+    /**
+     * 判断一个float数组是否已经排好序.
+     *
+     * @param target 待排序的float类型数组
+     * @param orderBy {@link Order#ASC}代表升序, {@link Order#DESC}代表降序
+     * @return 如果已排好序则返回 {@code true} 否则返回 {@code false}
+     * @since 2026.4.7
+     * @see ArraySortAlgorithmsUtility#isSorted(Comparable[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(Object[], Comparator, Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(byte[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(int[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(short[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(long[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(double[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(char[], Order)
+     */
+    public static boolean isSorted(float[] target, Order orderBy){
+        Objects.requireNonNull(target, "提供的数组不能为NULL");
+        boolean sorted = true;
+        for (int i = 0, j = i + 1; j < target.length; j++, i++) {
+            if (orderBy == Order.ASC && Float.compare(target[i], target[j]) > 0){
+                // 升序, 发现前面的大于后面的, 则sorted = false
+                sorted = false;
+                break;
+            }
+            if (orderBy == Order.DESC && Float.compare(target[i], target[j]) < 0){
+                sorted = false;
+                break;
+            }
+        }
+        return sorted;
+    }
+
+    /**
+     * 判断一个float数组是否已经排好序.
+     *
+     * @param target 待排序的float类型数组
+     * @param orderBy {@link Order#ASC}代表升序, {@link Order#DESC}代表降序
+     * @return 如果已排好序则返回 {@code true} 否则返回 {@code false}
+     * @since 2026.4.7
+     * @see ArraySortAlgorithmsUtility#isSorted(Comparable[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(Object[], Comparator, Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(byte[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(int[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(short[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(long[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(double[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(char[], Order)
+     */
+    public static boolean isSorted(int[] target, Order orderBy){
+        Objects.requireNonNull(target, "提供的数组不能为NULL");
+        boolean sorted = true;
+        for (int i = 0, j = i + 1; j < target.length; j++, i++) {
+            if (orderBy == Order.ASC && target[i] > target[j]){
+                // 升序, 发现前面的大于后面的, 则sorted = false
+                sorted = false;
+                break;
+            }
+            if (orderBy == Order.DESC && target[i] < target[j]){
+                sorted = false;
+                break;
+            }
+        }
+        return sorted;
+    }
+
+    /**
+     * 判断一个byte数组是否已经排好序.
+     *
+     * @param target 待排序的byte类型数组
+     * @param orderBy {@link Order#ASC}代表升序, {@link Order#DESC}代表降序
+     * @return 如果已排好序则返回 {@code true} 否则返回 {@code false}
+     * @since 2026.4.7
+     * @see ArraySortAlgorithmsUtility#isSorted(Comparable[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(Object[], Comparator, Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(float[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(int[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(short[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(long[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(double[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(char[], Order)
+     */
+    public static boolean isSorted(byte[] target, Order orderBy){
+        Objects.requireNonNull(target, "提供的数组不能为NULL");
+        boolean sorted = true;
+        for (int i = 0, j = i + 1; j < target.length; j++, i++) {
+            if (orderBy == Order.ASC && target[i] > target[j]){
+                // 升序, 发现前面的大于后面的, 则sorted = false
+                sorted = false;
+                break;
+            }
+            if (orderBy == Order.DESC && target[i] < target[j]){
+                sorted = false;
+                break;
+            }
+        }
+        return sorted;
+    }
+
+    /**
+     * 判断一个short数组是否已经排好序.
+     *
+     * @param target 待排序的short类型数组
+     * @param orderBy {@link Order#ASC}代表升序, {@link Order#DESC}代表降序
+     * @return 如果已排好序则返回 {@code true} 否则返回 {@code false}
+     * @since 2026.4.7
+     * @see ArraySortAlgorithmsUtility#isSorted(Comparable[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(Object[], Comparator, Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(float[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(int[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(byte[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(long[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(double[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(char[], Order)
+     */
+    public static boolean isSorted(short[] target, Order orderBy){
+        Objects.requireNonNull(target, "提供的数组不能为NULL");
+        boolean sorted = true;
+        for (int i = 0, j = i + 1; j < target.length; j++, i++) {
+            if (orderBy == Order.ASC && target[i] > target[j]){
+                // 升序, 发现前面的大于后面的, 则sorted = false
+                sorted = false;
+                break;
+            }
+            if (orderBy == Order.DESC && target[i] < target[j]){
+                sorted = false;
+                break;
+            }
+        }
+        return sorted;
+    }
+
+    /**
+     * 判断一个long数组是否已经排好序.
+     *
+     * @param target 待排序的long类型数组
+     * @param orderBy {@link Order#ASC}代表升序, {@link Order#DESC}代表降序
+     * @return 如果已排好序则返回 {@code true} 否则返回 {@code false}
+     * @since 2026.4.7
+     * @see ArraySortAlgorithmsUtility#isSorted(Comparable[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(Object[], Comparator, Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(float[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(int[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(byte[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(short[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(double[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(char[], Order)
+     */
+    public static boolean isSorted(long[] target, Order orderBy){
+        Objects.requireNonNull(target, "提供的数组不能为NULL");
+        boolean sorted = true;
+        for (int i = 0, j = i + 1; j < target.length; j++, i++) {
+            if (orderBy == Order.ASC && target[i] > target[j]){
+                // 升序, 发现前面的大于后面的, 则sorted = false
+                sorted = false;
+                break;
+            }
+            if (orderBy == Order.DESC && target[i] < target[j]){
+                sorted = false;
+                break;
+            }
+        }
+        return sorted;
+    }
+
+    /**
+     * 判断一个char数组是否已经排好序.
+     *
+     * @param target 待排序的char类型数组
+     * @param orderBy {@link Order#ASC}代表升序, {@link Order#DESC}代表降序
+     * @return 如果已排好序则返回 {@code true} 否则返回 {@code false}
+     * @since 2026.4.7
+     * @see ArraySortAlgorithmsUtility#isSorted(Comparable[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(Object[], Comparator, Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(float[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(int[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(byte[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(short[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(double[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(long[], Order)
+     */
+    public static boolean isSorted(char[] target, Order orderBy){
+        Objects.requireNonNull(target, "提供的数组不能为NULL");
+        boolean sorted = true;
+        for (int i = 0, j = i + 1; j < target.length; j++, i++) {
+            if (orderBy == Order.ASC && target[i] > target[j]){
+                // 升序, 发现前面的大于后面的, 则sorted = false
+                sorted = false;
+                break;
+            }
+            if (orderBy == Order.DESC && target[i] < target[j]){
+                sorted = false;
+                break;
+            }
+        }
+        return sorted;
+    }
+
+    /**
      * 判断一个数组是否已经排好序.
      *
      * <p>此方法提供给那些没有实现了{@link Comparable} 接口的类, 因此需要提供一个 {@link Comparator} 比较器来进行比较</p>
@@ -76,6 +342,13 @@ public class ArraySortAlgorithmsUtility {
      * @return 如果已排好序则返回 {@code true} 否则返回 {@code false}
      * @since 2026.4.7
      * @see ArraySortAlgorithmsUtility#isSorted(Comparable[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(int[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(byte[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(char[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(long[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(float[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(short[], Order)
+     * @see ArraySortAlgorithmsUtility#isSorted(double[], Order)
      */
     public static <T> boolean isSorted(T[] target, Comparator<T> comparator, Order orderBy){
         Objects.requireNonNull(target, "提供的数组不能为NULL");
