0cad90c0dc88a4fb87c5b1d4bec83617ff833540
yumitoy-askia <askiaxiaoming@yumitoy.com>
2026-04-15 15:03:07 +0800

    2026.4.15 添加Collection集合, Map结构的相关toString()方法

--- cocoa-utilities/src/main/java/cn/argento/askia/utilities/collection/CollectionUtility.java
+++ cocoa-utilities/src/main/java/cn/argento/askia/utilities/collection/CollectionUtility.java
@@ -12,14 +12,24 @@ import java.util.stream.Collectors;
 public class CollectionUtility {
 
     /**
-     * 增强类型的{@link Map#toString()}, 原版本的{@link Map#toString()}在遇到数组的时候只会打印出数组的引用, 此版本会对Map的value进行判断,
-     * 如果发现他是数组的话则会直接使用{@link Arrays#toString(Object[])}进行格式化后再拼接。
+     * 增强类型的{@link Map#toString()}.
      *
-     * @param map
-     * @return
+     * <p>原版本的{@link Map#toString()}在遇到数组的时候只会打印出数组的引用, 此版本会对{@link Map}的{@code value}进行判断, 如果发现他是数组的话则会直接使用{@link Arrays#deepToString(Object[])}进行格式化后再拼接输出</p>
+     *
+     * <p>调用者可以自行指定使用的分割器, 分割前缀和分割后缀, 方法底层使用{@link StringJoiner}进行字符串拼接来完成此方法的功能</p>
+     *
+     * @param map map结构
+     * @param delimiter 分割器文本
+     * @param begin 分割前缀
+     * @param end 分割后缀
+     * @return Map结构内部的Key, value内容组成的字符串
+     * @see CollectionUtility#toString(Map)
      */
-    public static String toString(Map<?, ?> map){
-        StringJoiner stringJoiner = new StringJoiner(", ", "[ ", " ]");
+    public static String toString(Map<?, ?> map, String delimiter, String begin, String end){
+        final String notNullDelimiter = Objects.requireNonNull(delimiter);
+        final String notNullBegin = Objects.requireNonNull(begin);
+        final String notNullEnd = Objects.requireNonNull(end);
+        StringJoiner stringJoiner = new StringJoiner(notNullDelimiter, notNullBegin, notNullEnd);
         final Set<? extends Map.Entry<?, ?>> entries = map.entrySet();
         for (Map.Entry<?, ?> entry : entries){
             final Object key = entry.getKey();
@@ -28,15 +38,122 @@ public class CollectionUtility {
             // 因为所有的引用类型都可以调用toString()维度数组不行，所以我们单独处理数组即可！
             if (value.getClass().isArray()){
                 final Object[] all = ArrayUtility.getAll(value);
-                final String s = Arrays.toString(all);
+                final String s = Arrays.deepToString(all);
                 keyValue += s;
             }
             else{
+                // 集合，map，list等是toString()方法仍然会打印出元素内容，因此可以不管
                 keyValue += value.toString();
             }
             stringJoiner.add(keyValue);
         }
         return stringJoiner.toString();
+    }
+    /**
+     * 增强类型的{@link Map#toString()}.
+     * <p>原版本的{@link Map#toString()}在遇到数组的时候只会打印出数组的引用, 此版本会对{@link Map}的{@code value}进行判断, 如果发现他是数组的话则会直接使用{@link Arrays#toString(Object[])}进行格式化后再拼接输出</p>
+     *
+     * <p>此方法默认使用<code>&#123; &#125;</code>和{@code ,}来进行分割, 此方法是{@link CollectionUtility#toString(Map, String, String, String)}的默认版本</p>
+     *
+     * @param map 任何类型的map
+     * @return Map结构内部的Key, value内容组成的字符串
+     */
+    public static String toString(Map<?, ?> map){
+        return toString(map, ", ", "{ ", " }");
+    }
+
+    /**
+     * 增强类型的{@link Collection#toString()}.
+     *
+     * <p>原版本的{@link Collection#toString()}在遇到数组的时候只会打印出数组的引用, 此版本会对值进行判断, 如果发现他是数组的话则会直接使用{@link Arrays#deepToString(Object[])}进行格式化后再拼接输出</p>
+     *
+     * <p>调用者可以自行指定使用的分割器, 分割前缀和分割后缀, 方法底层使用{@link StringJoiner}进行字符串拼接来完成此方法的功能</p>
+     *
+     * @param collectionTypeObj 任何集合类型, 比如{@link List}、{@link Set}、{@link Queue}以及他们的子接口和子实现
+     * @param delimiter 分割器
+     * @param begin 分割前缀
+     * @param end 分割后缀
+     * @param <C> 任何实现了{@link Collection}的接口或者实现类
+     * @return {@code Collection}内部元素toString结果
+     * @since 2026.4.14
+     * @see CollectionUtility#toString(Map)
+     * @see CollectionUtility#toString(Map, String, String, String)
+     * @see CollectionUtility#toString(Collection)
+     */
+    public static <C extends Collection<?>> String toString(C collectionTypeObj, String delimiter, String begin, String end){
+        final String notNullBegin = Objects.requireNonNull(begin);
+        final String notNullEnd = Objects.requireNonNull(end);
+        final String notNullDelimiter = Objects.requireNonNull(delimiter);
+        StringJoiner joiner = new StringJoiner(notNullDelimiter, notNullBegin, notNullEnd);
+        for (Object element : collectionTypeObj){
+            final Class<?> elementClass = element.getClass();
+            // 如果是数组, 则我们尝试使用数组进行输出
+            if (elementClass.isArray()){
+                final Object[] all = ArrayUtility.getAll(element);
+                final String s = Arrays.deepToString(all);
+                joiner.add(s);
+            }
+            else{
+                joiner.add(element.toString());
+            }
+        }
+        return joiner.toString();
+    }
+
+    /**
+     * 增强类型的{@link Collection#toString()}.
+     *
+     * <p>原版本的{@link Collection#toString()}在遇到数组的时候只会打印出数组的引用, 此版本会对值进行判断, 如果发现他是数组的话则会直接使用{@link Arrays#deepToString(Object[])}进行格式化后再拼接输出</p>
+     *
+     * <p>此方法在面对{@link Set}集合时, 使用<code>( )</code>来进行输出, 而其他如{@link List}、{@link Queue}等, 则使用<code>{ }</code>进行输出</p>
+     *
+     * <p>此方法为{@link CollectionUtility#toString(Collection, String, String, String)}的默认版本</p>
+     *
+     * @param collectionTypeObj 任何集合类型, 比如{@link List}、{@link Set}、{@link Queue}以及他们的子接口和子实现
+     * @param <C> 任何实现了{@link Collection}的接口或者实现类
+     * @return {@code Collection}内部元素toString结果
+     */
+    public static <C extends Collection<?>> String toString(C collectionTypeObj){
+        String prefix = "[";
+        String suffix = "]";
+        if (collectionTypeObj instanceof Set<?>){
+            prefix = "(";
+            suffix = ")";
+        }
+        return toString(collectionTypeObj, ", ", prefix, suffix);
+    }
+
+    public static <C extends Collection<?>> String toBeautifulString(C collectionTypeObj, boolean withSize, boolean withType){
+        final int size = collectionTypeObj.size();
+        final String collectionTypeClassName = collectionTypeObj.getClass().getName();
+        String prefix = collectionTypeClassName + " = [" + System.lineSeparator() + "\t\t\t";
+        String delimiter = "," + System.lineSeparator() + "\t\t\t";
+        if (!withType){
+            prefix = "[" + System.lineSeparator() + "\t\t\t";
+        }
+        String suffer = System.lineSeparator() + "], size = " + size;
+        if (!withSize){
+            suffer = System.lineSeparator() + "]";
+        }
+        final String notNullBegin = Objects.requireNonNull(prefix);
+        final String notNullEnd = Objects.requireNonNull(suffer);
+        final String notNullDelimiter = Objects.requireNonNull(delimiter);
+        StringJoiner joiner = new StringJoiner(notNullDelimiter, notNullBegin, notNullEnd);
+        for (Object element : collectionTypeObj){
+            final Class<?> elementClass = element.getClass();
+            // 如果是数组, 则我们尝试使用数组进行输出
+            if (elementClass.isArray()){
+                final String s = ArrayUtility.toBeautifulString(element);
+                joiner.add(s);
+            }
+            else{
+                joiner.add(element.toString());
+            }
+        }
+        return joiner.toString();
+    }
+    public static <C extends Collection<?>> String toBeautifulString(C collectionTypeObj){
+        return toBeautifulString(collectionTypeObj, false, false);
     }
 
 
@@ -219,7 +336,15 @@ public class CollectionUtility {
         list.retainAll(list2);
         return list;
     }
-    // 判断两个集合是否是包含关系
+
+    /**
+     * 判断两个集合是否是包含关系.
+     * <p>比如
+     * @param list List结构
+     * @param list2
+     * @param <T>
+     * @return
+     */
     public static <T> boolean isInclusionOriginal(List<T> list, List<T> list2){
         if (list.size() < list2.size()){
             return !list.retainAll(list2);
@@ -501,6 +626,11 @@ public class CollectionUtility {
     }
 
     public static void main(String[] args) {
+        final Integer[][] integers = ArrayUtility.newMatrix(10, 10);
+        final CharSequence[][] integers1 = ArrayUtility.newArrayInstance(String[].class, 20, new String[]{"123", "456", "789"}, new String[]{"222", "333", "444"});
+
+        System.out.println(Arrays.deepToString(integers1));
+        System.out.println(Arrays.deepToString(new java.lang.String[]{"123", "456", "789"}));
         List<Integer> list = new ArrayList<>();
         list.add(3);
         list.add(2);
@@ -511,6 +641,6 @@ public class CollectionUtility {
         list2.add(3);
         list2.add(4);
         list2.add(5);
-        System.out.println(equals(list, list2));
+        System.out.println(toBeautifulString(list, false, false));
     }
 }
