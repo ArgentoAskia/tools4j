26f526bc040db3dbd9103518d6fe96cbcff4ae90
yumitoy-askia <askiaxiaoming@yumitoy.com>
2026-04-23 10:22:15 +0800

    2026.4.23 补充原子操作接口文档

--- cocoa-cache/src/main/java/cn/argento/askia/cache/support/AtomicSupport.java
+++ cocoa-cache/src/main/java/cn/argento/askia/cache/support/AtomicSupport.java
@@ -3,7 +3,11 @@ package cn.argento.askia.cache.support;
 import java.util.function.BinaryOperator;
 
 /**
- * 缓存原子操作支持
+ * 缓存原子操作支持.
+ *
+ * <p>提供CompareAndSet、getAndSet、getAndDelete、</p>
+ *
+ * <p>需要提供原子操作支持的缓存可以实现此接口</p>
  */
 public interface AtomicSupport<K, V> {
     boolean compareAndSet(K key, V expired, V update);
