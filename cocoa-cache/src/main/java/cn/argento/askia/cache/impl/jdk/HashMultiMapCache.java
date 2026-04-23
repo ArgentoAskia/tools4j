3bc885f645f9f1e9c54c2748d8285dbf0c790c22
ArgentoAskia <Askiaxiaoming@outlook.com>
2026-04-23 10:51:04 +0800

    2026.4.23 HashMultiMapCache添加委托

--- cocoa-cache/src/main/java/cn/argento/askia/cache/impl/jdk/HashMultiMapCache.java
+++ cocoa-cache/src/main/java/cn/argento/askia/cache/impl/jdk/HashMultiMapCache.java
@@ -1,4 +1,27 @@
 package cn.argento.askia.cache.impl.jdk;
 
-public class HashMultiMapCache {
+import cn.argento.askia.cache.support.MapValueSupport;
+
+import java.util.Map;
+
+public class HashMultiMapCache<K, K2, V> extends AbstractHashMapCacheDelegation<K, Map<K2, V>> implements MapValueSupport<K2, V> {
+    private HashMapCache<K, Map<K2, V>> mapHashMapCache;
+
+    public HashMultiMapCache(){
+        mapHashMapCache = new HashMapCache<>();
+    }
+
+    public HashMultiMapCache(int initialCapacity, float loadFactor){
+        mapHashMapCache = new HashMapCache<>(initialCapacity, loadFactor);
+    }
+    public HashMultiMapCache(int initialCapacity){
+        mapHashMapCache = new HashMapCache<>(initialCapacity);
+    }
+
+
+
+    @Override
+    protected HashMapCache<K, Map<K2, V>> delegate() {
+        return mapHashMapCache;
+    }
 }
