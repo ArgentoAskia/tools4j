30e5aee1596447522840e9e247250bf573a2ba80
yumitoy-askia <askiaxiaoming@yumitoy.com>
2026-04-26 15:23:59 +0800

    2026.4.26 补充@Utility相关的文档

--- cocoa-core/src/main/java/cn/argento/askia/annotations/Utility.java
+++ cocoa-core/src/main/java/cn/argento/askia/annotations/Utility.java
@@ -20,7 +20,7 @@ import java.lang.annotation.*;
 public @interface Utility {
 
     /**
-     * 描述此工具类的用途的文本
+     * 描述此工具类的用途的文本(名称)
      * @return 描述文本
      */
     String value();
@@ -43,4 +43,25 @@ public @interface Utility {
      * @return 作者信息
      */
     String author() default "Unknown";
+
+
+//    /**
+//     * 工具类的描述！
+//     *
+//     * @return 工具类的描述
+//     */
+//    String description() default "";
+//
+//    /**
+//     * 描述文件的具体位置，该选项主要防止{@link Utility#description()}过长导致注解过于臃肿而将具体的描述信息外移！
+//     * 也可以将{@link Utility#description()}作为简短说明而本属性作为长说明使用！
+//     *
+//     * 文件位置支持："classpath:"选项,因此可以这样路径：
+//     * <code>
+//     *      @Utility(descriptionFileURL = "classpath:statement.txt")
+//     * </code>
+//     *
+//     * @return 描述文件的路径！
+//     */
+//    String descriptionFileURL() default "";
 }
