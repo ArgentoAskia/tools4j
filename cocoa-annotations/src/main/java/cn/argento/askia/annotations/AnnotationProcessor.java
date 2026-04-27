d2dc776d795ebef33a59eec4694c46b1ccfefe21
yumitoy-askia <askiaxiaoming@yumitoy.com>
2026-04-27 11:42:40 +0800

    2026.4.27 添加注解处理器注解

--- cocoa-annotations/src/main/java/cn/argento/askia/annotations/AnnotationProcessor.java
+++ cocoa-annotations/src/main/java/cn/argento/askia/annotations/AnnotationProcessor.java
@@ -8,11 +8,39 @@ import java.lang.annotation.*;
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD, ElementType.TYPE, ElementType.FIELD})
+@Target({ElementType.ANNOTATION_TYPE, ElementType.TYPE})
 public @interface AnnotationProcessor {
 
-    Class<? extends cn.argento.askia.processors.AnnotationProcessor> value()
-            default cn.argento.askia.processors.AnnotationProcessor.class;
+    /**
+     *
+     * @return
+     * @see #resolves()
+     * @see #binds()
+     */
+    Class<?>[] value() default {};
 
-    Class<? extends Annotation> annotationClass() default Annotation.class;
+    /**
+     * 此处理器关心的注解类型（必须至少指定一个），指定此处的值，则证明该注解处理器的所有阶段都能处理该注解
+     *
+     * @return 注解
+     */
+    Class<? extends Annotation>[] resolves() default {};
+
+    /**
+     * 绑定哪个注解处理器, 仅当该注解标记在注解上时有效
+     * @return 注解处理器
+     */
+    Class<?>[] binds() default {};
+
+
+    /**
+     * 注解处理器级的执行顺序（数值越小优先级越高，越早执行）安排
+     * 默认 0，可支持负值
+     */
+    int order() default 0;
+
+    /**
+     * 处理器名称（若不指定，默认使用类的简单名称）
+     */
+    String name() default "";
 }
