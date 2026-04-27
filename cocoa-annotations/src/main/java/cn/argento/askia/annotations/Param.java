4377f6a9b27b8d8bb39624deeac33befb8cd3315
yumitoy-askia <askiaxiaoming@yumitoy.com>
2026-04-27 12:35:41 +0800

    2026.4.27 添加参数注入注解

---
+++ cocoa-annotations/src/main/java/cn/argento/askia/annotations/Param.java
@@ -0,0 +1,30 @@
+package cn.argento.askia.annotations;
+
+import cn.argento.askia.supports.LifeCyclePhase;
+import cn.argento.askia.supports.Phase;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 参数注入注解.
+ * 默认按照类型进行注入, 当类型发生冲突时按照名称进行注入, 如果没有指定名称则抛出异常
+ */
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Param {
+
+    /**
+     * 上下文参数名
+     * @return 参数名
+     */
+    String value() default "";
+
+    /**
+     * 获取阶段结束的返回值
+     * @return 阶段结束的返回值
+     */
+    Phase phaseRet() default Phase.LAST_PHASE;
+}
