9ed58587f450fe062b96b138a1edbd5ce0723440
yumitoy-askia <askiaxiaoming@yumitoy.com>
2026-04-27 11:42:18 +0800

    2026.4.27 注解处理框架各个阶段注解设计

---
+++ cocoa-annotations/src/main/java/cn/argento/askia/annotations/phase/PostProcessingPhase.java
@@ -0,0 +1,40 @@
+package cn.argento.askia.annotations.phase;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Repeatable(PostProcessingPhase.PostProcessingPhaseContainer.class)
+public @interface PostProcessingPhase {
+
+    /**
+     * 绑定哪个注解处理器
+     * @return
+     */
+    String bind() default "";
+
+    /**
+     * 执行顺序（数值越小优先级越高，越早执行）
+     * 默认 0，可支持负值
+     */
+    int order() default 0;
+
+
+    /**
+     * 此处理器关心的注解类型（必须至少指定一个），不提供则使用顶层的@AnnotationProcessor的值
+     */
+    Class<? extends Annotation>[] value() default {};
+
+    /**
+     * 指定是否完全覆盖顶层的@AnnotationProcessor的值.
+     * @return 默认覆盖
+     */
+    boolean override() default true;
+
+
+    @Target(ElementType.METHOD)
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface PostProcessingPhaseContainer{
+        PostProcessingPhase[] value();
+    }
+}
