cfb69e8429ea5ffcd0ba0be01df9cbf275fc2e38
yumitoy-askia <askiaxiaoming@yumitoy.com>
2026-05-02 00:11:01 +0800

    2026.5.2 注解处理器初始化注解

---
+++ cocoa-annotations/src/main/java/cn/argento/askia/annotations/Initialization.java
@@ -0,0 +1,17 @@
+package cn.argento.askia.annotations;
+
+
+import javax.annotation.PostConstruct;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Initialization {
+
+}
