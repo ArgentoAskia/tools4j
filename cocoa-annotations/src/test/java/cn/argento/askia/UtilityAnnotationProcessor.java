3a7d1605e5fa024d3246c072a1e419bfa9250832
yumitoy-askia <askiaxiaoming@yumitoy.com>
2026-05-10 17:50:47 +0800

    2026.5.10 编写@Utility注解处理器

--- cocoa-annotations/src/test/java/cn/argento/askia/UtilityAnnotationProcessor.java
+++ cocoa-annotations/src/test/java/cn/argento/askia/UtilityAnnotationProcessor.java
@@ -4,16 +4,26 @@ package cn.argento.askia;
 import cn.argento.askia.annotations.AnnotationProcessor;
 import cn.argento.askia.annotations.Utility;
 import cn.argento.askia.annotations.phase.*;
+import cn.argento.askia.context.MutableAnnotationProcessorContext;
+import cn.argento.askia.scanner.DefaultFileScanBasedEnvironmentScanner;
+import cn.argento.askia.supports.environment.FileScanBasedEnvironmentBean;
 
 import java.util.List;
 import java.util.Set;
 
+/**
+ * {@code &#64;Utility} 处理器
+ *
+ */
 @AnnotationProcessor(Utility.class)
 public class UtilityAnnotationProcessor {
 
 
     @ScanPhase
-    public int scanPhase(){
+    public int scanPhase(MutableAnnotationProcessorContext context){
+
+        DefaultFileScanBasedEnvironmentScanner scanner = new DefaultFileScanBasedEnvironmentScanner();
+
         return 0;
     }
 
