4fa4a4f7088b034ca0fa8d361f514bbcc636a76b
yumitoy-askia <askiaxiaoming@yumitoy.com>
2026-05-12 21:14:34 +0800

    2026.5.12 迁移initContext()、closeContext()等方法

--- cocoa-annotations/src/main/java/cn/argento/askia/annotations/AnnotationProcessorContextHelper.java
+++ cocoa-annotations/src/main/java/cn/argento/askia/annotations/AnnotationProcessorContextHelper.java
@@ -2,11 +2,23 @@ package cn.argento.askia.annotations;
 
 
 import cn.argento.askia.annotations.ApiStatus;
+import cn.argento.askia.annotations.annotation.Destroy;
+import cn.argento.askia.annotations.annotation.Initialization;
+import cn.argento.askia.annotations.annotation.Param;
 import cn.argento.askia.annotations.context.AnnotationProcessorContext;
 import cn.argento.askia.annotations.context.BeanNotFoundException;
 import cn.argento.askia.annotations.context.MoreThenOneBeanException;
 import cn.argento.askia.annotations.context.MutableAnnotationProcessorContext;
+import cn.argento.askia.utilities.annotation.AnnotationUtility;
 import cn.argento.askia.utilities.lang.StringUtility;
+import cn.argento.askia.utilities.reflect.ReflectUtility;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.Set;
 
 
 @ApiStatus.Internal
@@ -52,6 +64,140 @@ class AnnotationProcessorContextHelper {
                     throw new BeanNotFoundException("找不到名字为" + beanName + "的Bean, 因此抛出异常", e);
                 }
                 return beanByName;
+            }
+        }
+    }
+
+
+    /**
+     * 初始化上下文容器
+     * @param context context
+     */
+    static void initContext(MutableAnnotationProcessorContext context, Object annotationProcessObj) throws InvocationTargetException, IllegalAccessException {
+        final Class<?> annotationProcessObjClass = annotationProcessObj.getClass();
+        final Set<Method> declaredMethods = ReflectUtility.getAllMethods(annotationProcessObjClass);
+        Method initMethod = null;
+        String beanName = null;
+        for (Method m : declaredMethods){
+            if (AnnotationUtility.isDeclaredAnnotationPresent(m, Initialization.class)) {
+                // 标记了@Initialization
+                initMethod = m;
+                beanName = Initialization.class.getSimpleName();
+                break;
+            }
+            if (AnnotationUtility.isDeclaredAnnotationPresent(m, PostConstruct.class)){
+                // 标记了@PostConstruct
+                initMethod = m;
+                beanName = PostConstruct.class.getSimpleName();
+                break;
+            }
+        }
+        if (initMethod != null){
+            // 允许访问私有方法和继承性方法
+            if (!initMethod.isAccessible()){
+                initMethod.setAccessible(true);
+            }
+            final Class<?>[] parameterTypes = initMethod.getParameterTypes();
+            if (parameterTypes.length == 0){
+                // 直接调用
+                final Object invoke = initMethod.invoke(annotationProcessObj);
+                context.registerBean(beanName, invoke);
+                return;
+            }
+            if (parameterTypes.length == 1 && (AnnotationProcessorContext.class == parameterTypes[0] || MutableAnnotationProcessorContext.class == parameterTypes[0])){
+                final Object invoke = initMethod.invoke(annotationProcessObj, context);
+                context.registerBean(beanName, invoke);
+                return;
+            }
+            // 两个以上的参数
+            if (parameterTypes.length > 1){
+                final Parameter[] parameters = initMethod.getParameters();
+                Object[] args = new Object[parameters.length];
+                int index = 0;
+                for (Parameter p : parameters){
+                    if (p.getType() == AnnotationProcessorContext.class || p.getType() == MutableAnnotationProcessorContext.class){
+                        args[index++] = context;
+                        continue;
+                    }
+                    String paramName = "";
+                    if (p.isAnnotationPresent(Param.class)){
+                        final Param declaredAnnotation = p.getDeclaredAnnotation(Param.class);
+                        paramName = declaredAnnotation.value();
+                    }
+                    try{
+                        final Object bean = AnnotationProcessorContextHelper.findBean(p.getType(), paramName, context);
+                        args[index++] = bean;
+                    }
+                    catch (BeanNotFoundException e){
+                        //  找不到填null
+                        args[index++] = null;
+                    }
+                }
+                final Object invoke = initMethod.invoke(annotationProcessObj, args);
+                // 如果方法返回值不等于void或者Void, 则我们尝试将返回值放入容器中, 哪怕返回值本身就是null
+                if (initMethod.getReturnType() != void.class && initMethod.getReturnType() != Void.class){
+                    context.registerBean(beanName, invoke);
+                }
+            }
+        }
+    }
+    // 只读容器不能操作，因此不需要关闭容器
+    static void initReadOnlyContext(AnnotationProcessorContext context, Object annotationProcessObj){
+
+    }
+
+    static void closeContext(MutableAnnotationProcessorContext context, Object annotationProcessObj) throws InvocationTargetException, IllegalAccessException {
+        final Class<?> annotationProcessObjClass = annotationProcessObj.getClass();
+        final Set<Method> declaredMethods = ReflectUtility.getAllMethods(annotationProcessObjClass);
+        Method destroyMethod = null;
+        String beanName = null;
+        for (Method m : declaredMethods){
+            if (AnnotationUtility.isDeclaredAnnotationPresent(m, Destroy.class)) {
+                // 标记了@Initialization
+                destroyMethod = m;
+                beanName = Destroy.class.getSimpleName();
+                break;
+            }
+            if (AnnotationUtility.isDeclaredAnnotationPresent(m, PreDestroy.class)){
+                // 标记了@PostConstruct
+                destroyMethod = m;
+                beanName = PreDestroy.class.getSimpleName();
+                break;
+            }
+        }
+        if (destroyMethod != null){
+            // 允许访问私有方法和继承性方法
+            if (!destroyMethod.isAccessible()){
+                destroyMethod.setAccessible(true);
+            }
+            final Class<?>[] parameterTypes = destroyMethod.getParameterTypes();
+            if (parameterTypes.length == 0){
+                // 直接调用
+                final Object invoke = destroyMethod.invoke(annotationProcessObj);
+                context.registerBean(beanName, invoke);
+                return;
+            }
+            if (parameterTypes.length == 1 && (AnnotationProcessorContext.class == parameterTypes[0] || MutableAnnotationProcessorContext.class == parameterTypes[0])){
+                final Object invoke = destroyMethod.invoke(annotationProcessObj, context);
+                context.registerBean(beanName, invoke);
+                return;
+            }
+            // 两个以上的参数
+            if (parameterTypes.length > 1){
+                final Parameter[] parameters = destroyMethod.getParameters();
+                Object[] args = new Object[parameters.length];
+                int index = 0;
+                for (Parameter p : parameters){
+                    if (p.getType() == AnnotationProcessorContext.class || p.getType() == MutableAnnotationProcessorContext.class){
+                        args[index++] = context;
+                        continue;
+                    }
+                    args[index++] = null;
+                }
+                final Object invoke = destroyMethod.invoke(annotationProcessObj, args);
+                if (destroyMethod.getReturnType() != void.class && destroyMethod.getReturnType() != Void.class){
+                    context.registerBean(beanName, invoke);
+                }
             }
         }
     }
